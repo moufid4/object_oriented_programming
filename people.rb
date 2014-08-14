cf5dea1da0b6061e9bcb66cef022d5ac0ca68c95
Moufid Larbi <moufid4@gmail.com>
2014-08-14 14:14:49 -0400

    Step 2 : Add method called learn that outputs 'I get it'

---
+++ people.rb
@@ -0,0 +1,21 @@
+class Student
+
+	def initialize
+
+	end
+
+
+	# Step 2 : The student class has a method called learn that outputs "I get it!"
+	def learn
+
+		puts "I get it!"
+	end
+
+
+end
+
+class Instructor
+
+
+
+end
