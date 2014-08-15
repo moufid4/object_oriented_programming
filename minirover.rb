7943c35f9704b05d157625d377ac6f02e8019372
Moufid Larbi <moufid4@gmail.com>
2014-08-15 14:57:14 -0400

    Errors correction

--- minirover.rb
+++ minirover.rb
@@ -11,49 +11,41 @@ class MiniRover
 
 
 	def current
-		puts "(#{@x}, #{@y}, #{@d})"
+		puts "The rover's current position (#{@x}, #{@y}, #{@d})"
 	end
 
 
 	def turn(instruction)
 
 		if instruction == "L" && @d == "N"
-			puts "The rover will head WEST"
 			@d = "W"
 			return
 		end	
 		if instruction == "L" && @d == "S"
-			puts "The rover will head EAST"
 			@d = "E"
 			return
 		end
 		if instruction == "L" && @d == "E"
-			puts "The rover will head NORTH"
 			@d = "N"
 			return	
 		end
 		if instruction == "L" && @d == "W"
-			puts "The rover will head SOUTH"
 			@d = "S"
 			return
 		end
 		if instruction == "R" && @d == "N"
-			puts "The rover will head EAST"
 			@d = "E"
 			return
 		end
 		if instruction == "R" && @d == "S"
-			puts "The rover will head WEST"
 			@d = "W"
 			return
 		end
 		if instruction == "R" && @d == "E"
-			puts "The rover will head SOUTH"
 			@d = "S"
 			return
 		end		
 		if instruction == "R" && @d == "W"
-			puts "The rover will head NORTH"
 			@d = "N"
 			return
 		end
@@ -89,14 +81,12 @@ class UserInput
 		@x = @position.split(//)[1].to_i
 		@y = @position.split(//)[3].to_i
 		@d = @position.split(//)[5].upcase	
-		puts "#{@position.split(//)}"
 		@rover1 = MiniRover.new(@x,@y,@d)
 	end
 
 	def self.input_instructions
 		puts "Instructions? LLMLMLML"
 		@instructions = gets.chomp.upcase
-		puts "Given instructions #{@instructions.split(//)}"
 		@instructions.split(//).each do |instruction|
 			if instruction == "M"
 				@rover1.move
